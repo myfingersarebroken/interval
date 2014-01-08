023aa12cd9bf080ba7f75c58e4952c86e57166a0
Fernando Faria <fernando.al.faria@gmail.com>
2014-01-08 12:33:38 -0200

    Update interval.js

--- interval.js
+++ interval.js
@@ -1,33 +1,34 @@
 /**
  * @function interval
- * @param func {Function} The function to execute
- * @param wait {Number} The milisseconds between excutions
- * @param times {Number} Maximum number of excutions
+ * @param {Function} func - The function to execute
+ * @param {Number} wait - The milisseconds between excutions
+ * @param {Number} times - Maximum number of excutions
+ * @param {Object} context - Optional; A context to apply the function if needed
  * @return {Object} An object with a clear() method to stop the interval
  * @author Fernando Faria
  */
-function interval(func, wait, times) {
-	var interv = (function(w, t) {
-		function i() {
-			if (typeof t === 'undefined' || t-- > 0) {
-				setTimeout(interv, w);
-				
-				try {
-					func.call(null);
-				} catch(e) {
-					t = 0;
-					
-					throw e.toString();
-				}
-			}
-		}
-		
-		i.clear = function() { t = 0; return null; }
-		
-		return i;
-	})(wait, times);
-	
-	setTimeout(interv, wait);
-	
-	return { clear : interv.clear }
+function interval(func, wait, times, context) {
+        var interv = (function(w, t) {
+                function i() {
+                        if (typeof t === 'undefined' || t-- > 0) {
+                                setTimeout(interv, w);
+                                
+                                try {
+                                        func.call((context || null));
+                                } catch(e) {
+                                        t = 0;
+                                        
+                                        throw e.toString();
+                                }
+                        }
+                }
+                
+                i.clear = function() { t = 0; return null; }
+                
+                return i;
+        })(wait, times);
+        
+        setTimeout(interv, wait);
+        
+        return { clear : interv.clear }
 }
