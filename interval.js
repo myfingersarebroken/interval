ffa6515df798fbde422a6f8d07f5c805da0b4274
Fernando Faria <fernando.al.faria@gmail.com>
2014-06-03 21:12:26 -0300

    Update interval.js

--- interval.js
+++ interval.js
@@ -1,123 +1,137 @@
-function _interval(func, wait, times) {
-	// Máquina de estados para o ciclo de assíncronia
-	var stateMachine = {
-		  maxCalls : times || Number.POSITIVE_INFINITY
-		, actualCall : 0
-		, data : []
-		, error : []
-		, continue : function() {}
-		, stop : function() {}
-		, clear : function() {}
-		, stopOnError : true
-	};
-	
-	// utilizamos este closure para blindar o escopo da stateMachine
-var interv = (function(w, t) {
-		function i() {
-			if (t-- > 0) {
-				try {
-					// o retorno computado é armazenado em stateMachine.data
-					i.state.data[stateMachine.actualCall] = func.call(i, stateMachine);
-					
-					// se não ocorrerem erros, incremento o ciclo
-					stateMachine.actualCall += 1;
-				} catch(e) {
-					/* Se ocorrer algum tipo de exceção paramos a execução.
-					 * Podemos configurar isso já na primeira execução de qualquer chamada de _async.
-					 *
-					 * @example
-					 *		var someAsync = _async(function(state) {
-					 *			state.stopOnError = false;
-					 *		}, 300, 10);
-					 */
-					if (stateMachine.stopOnError) {
-						t = 0;
+/**
+	 * A safe setTimeout / setInterval implementation
+	 * Visando melhorar a interface de timeout e ter um retorno do stack trace adequado, as chamadas assíncronas sempre serão executadas
+	 * no contexto de uma state machine.
+	 * Essa state machine sempre é retornada para a variável que armazena _async, assim, permitindo maior controle sobre o que ocorre no programa.
+	 * A rotina passada para _async poderá ter cláusula 'return', esta por sua vez sendo armazenada em sua_variavel.state.data['indice_da_chamada_em_que_ocorreu_a_rotina'].
+	 * A respeito da state machine, ela também é repassada como parâmetro implícito para a rotina passada para _async, sendo possível manipulações
+	 * de dentro da rotina, obtendo maior controle sobre o fluxo de execução.
+	 * Note que a state machine referente à rotina assíncrona é um objeto e, logo, se precisarmos de variáveis compartilhadas entre as chamadas, devemos
+	 * defini-las como atributos de 'state', ao invés de declará-las com a palavra-chave 'var'.
+	 *
+	 * @example
+	 *		var myAsync = _async(function(state) {
+	 *			// Detro desta rotina, o parâmetro intrínseco 'state' representa a state machine desta mesma rotina.
+	 *			// Esta state machine também pode ser referenciada pela variável myAsync através de myAsync.state.
+	 *			// Os atributos customizados definidos para 'state' são compartilhados entre as chamadas, logo, se definirmos um atributo
+	 *			// state.myName, podemos acessá-lo nas próximas chamadas tanto por state.myName quanto por myAsync.state.myName.
+	 *			// Para não sobrescrever o atributo a cada ciclo de excução da rotina, como boa prática sempre verificaremos se o atributo
+	 *			// já existe, como segue abaixo:
+	 *			if (!state.mySharedAttribute) {
+	 *				state.mySharedAttribute = 'my value';
+	 *			}
+	 *		}, 250, 35);
+	 *
+	 *
+	 * @example fibonnaci
+	 *		// apenas para ilustrar as possibilidades de computação
+	 *		var fib = _async(function(state) {
+	 *			if(state.actualCall == 0) {
+	 *				return 0;
+	 *			} else if(state.actualCall == 1) {
+	 *				return 1;
+	 *			} else {
+	 *				return state.data[state.actualCall - 1] + state.data[state.actualCall - 2];
+	 *			}
+	 *		}, 1000);
+	 *
+	 *
+	 * @function _interval
+	 * @param {_asyncRoutine} func - The routine to execute
+	 * @param {Number} wait - The milisseconds between excutions
+	 * @param {Number} times - Maximum number of excutions
+	 * @return {Object} A state machine of the asynchronous calls
+	 * @author Fernando Faria - cin_ffaria@uolinc.com
+	 */
+	function _interval(func, wait, times) {
+		// Máquina de estados para o ciclo de assíncronia
+		var stateMachine = {
+			  maxCalls : times || Number.POSITIVE_INFINITY
+			, actualCall : 0
+			, data : []
+			, lastComputedData : null
+			, error : []
+			, lastComputedError : null
+			, stop : function() {}
+			, clear : function() {}
+			, stopOnError : true
+		};
+		
+		// utilizamos este closure para blindar o escopo da stateMachine
+        var interv = (function(w, t) {
+			function i() {
+				if (t-- > 0) {
+					try {
+						// o retorno computado é armazenado em stateMachine.data
+						i.state.data[stateMachine.actualCall] = func.call(i, stateMachine);
+						i.state.lastComputedData = i.state.data[stateMachine.actualCall];
+						
+						// se não ocorrerem erros, incremento o ciclo
+						stateMachine.actualCall += 1;
+					} catch(e) {
+						/* Se ocorrer algum tipo de exceção paramos a execução.
+						 * Podemos configurar isso já na primeira execução de qualquer chamada de _async.
+						 *
+						 * @example
+						 *		var someAsync = _async(function(state) {
+						 *			state.stopOnError = false;
+						 *		}, 300, 10);
+						 */
+						if (stateMachine.stopOnError) {
+							t = 0;
+						}
+						
+						// salvamos o erro em stateMachine.error
+						i.state.error[stateMachine.actualCall] = e;
+						i.state.lastComputedError = i.state.error[stateMachine.actualCall];
+						
+						/* deixamos que o desenvlvedor decida o que fazer quando o ocorrer algum erro e por isso
+						 * executamos uma última vez a função passada 'func'
+						 *
+						 * @example
+						 */
+						try {
+							/* Perceba que pode-se retornar um novo _async a qualquer momento bastando apenas atualizar a stateMachine, como no último exemplo
+							 * mas deve-se tomar cuidado caso deseje-se continuar os ciclos mesmo após alguma exceção, pois podemos perder a referência à stateMachine
+							 * anterior e nunca mais conseguiremos parar as chamadas
+							 */
+							i.state.data[stateMachine.actualCall] = func.call(i, stateMachine);
+							i.state.lastComputedData = i.state.data[stateMachine.actualCall];
+							
+							// se estiver configurado para não para a execução mesmo com erros, continuamos a incrementar os ciclos
+							stateMachine.actualCall += 1
+						} catch(er) {}
 					}
 					
-					// salvamos o erro em stateMachine.error
-					i.state.error[stateMachine.actualCall] = e;
-					
-					/* deixamos que o desenvlvedor decida o que fazer quando o ocorrer algum erro e por isso
-					 * executamos uma última vez a função passada 'func'
-					 *
-					 * @example
-					 *		var someAsync = _async(function(state1) {
-					 *			if (state1.error) {
-					 *				return _async(function(state2) {
-					 *					// pode fazer alguma coisa
-					 *					state1 = state2; // atualiza a referêcia para a stateMachine atual
-					 *				}, 300, 5);
-					 *			}
-					 *		}, 300, 12);
-					 */
-					try {
-						/* Perceba que pode-se retornar um novo _async a qualquer momento bastando apenas atualizar a stateMachine, como no último exemplo
-						 * mas deve-se tomar cuidado caso deseje-se continuar os ciclos mesmo após alguma exceção, pois podemos perder a referência à stateMachine
-						 * anterior e nunca mais conseguiremos parar as chamadas
-						 *
-						 * @example
-						 *		var myAsync = _async(function(state1) {
-						 *			if (state1.error) {
-						 *				return _async(function(state2) {
-						 *					if (state1 != state2) {
-						 *						// veja que é uma boa prática atualizar a referência da stateMachine
-						 *						// mas se não fizer, a state2 somente será referenciada através de myAsync.state.data.state
-						 *						state1 = state2;
-						 *					}
-						 *					return 3;
-						 *				});
-						 *			}
-						 *		}, 350, 8);
-						 *
-						 * @example
-						 *		var myAsync2 = _async(function(state1) {
-						 *			// aqui, não vamos finalizar o _async quando alguma exceção for lançada
-						 *			state1.stopOnError = false;
-						 *			
-						 *			if (state1.error) {
-						 *				return _async(function(state2) {
-						 *					// perceba que se atualizarmos a referência para a stateMachine2,
-						 *					// perdemos a stateMachine1 e não poderemos mais finalizá-la
-						 *					if (state1 != state2) {
-						 *						state1 = state2;
-						 *					}
-						 *				}, 100, 500);
-						 *			}
-						 *		}, 400, 7);
-						 */
-						i.state.data[stateMachine.stopOnError ? stateMachine.actualCall : (stateMachine.actualCall += 1)] = func.call(i, stateMachine);
-						
-						// se estiver configurado para não para a execução mesmo com erros, continuamos a incrementar os ciclos
-						/*if (!stateMachine.stopOnError) {
-							stateMachine.cycles++;
-						}*/
-					} catch(er) {}
+					setTimeout(interv, w);
 				}
-				
-				setTimeout(interv, w);
 			}
-		}
+			
+			// definimos agora a função de finalização do _async
+			stateMachine.clear = function() { t = 0; return null; };
+			// repassamos uma referência de stateMachine para manipulação fora do closure
+			i.state = stateMachine;
+			// apenas mais uma referência para o método clear()
+			i.clear = stateMachine.clear;
+			
+			return i;
+        })(wait, times || Number.POSITIVE_INFINITY);
+        
+        setTimeout(interv, wait);
 		
-		// definimos agora a função de finalização do _async
-		stateMachine.clear = function() { t = 0; return null; };
-		// repassamos uma referência de stateMachine para manipulação fora do closure
-		i.state = stateMachine;
-		// apenas mais uma referência para o método clear()
-		i.clear = stateMachine.clear;
-		
-		return i;
-})(wait, times);
-
-setTimeout(interv, wait);
+        return interv;
+	} /* @end _interval */
 	
-return interv;
-}
-
-/**
- * A facade for _interval
- *
- * @function _async
- */
-function _async(func, wait, times) {
-	return _interval(func, wait, times || Number.POSITIVE_INFINITY);
-}
+	/**
+	 * @callback _asyncRoutine
+	 * @param {Object} state - Uma state machine compartilhada entre os ciclos de _async
+	 */
+	
+	/**
+	 * A facade for _interval
+	 *
+	 * @function _async
+	 */
+	function _async(func, wait, times) {
+		return _interval(func, wait, times);
+	}
